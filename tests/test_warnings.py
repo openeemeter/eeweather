f0a4bc2d3f3113448aead0ddbe4c2ad617e5e85d
Stephen Suffian <steve@openee.io>
2018-08-20 19:15:34 -0400

    changed test name

--- tests/test_warnings.py
+++ tests/test_warnings.py
@@ -14,6 +14,9 @@ def test_str_repr(generic_eeweather_warning):
     assert str(generic_eeweather_warning) == (
         'EEWeatherWarning(qualified_name=qualified_name)'
     )
+
+
+def test_json_repr(generic_eeweather_warning):
     assert generic_eeweather_warning.json() == {
         'qualified_name': 'qualified_name',
         'description': 'description',
