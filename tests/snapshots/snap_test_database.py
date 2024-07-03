981490836704228cc571b08326407d2e6dab602d
Stephen Suffian <stephen.suffian@pm.me>
2024-07-03 09:58:19 -0400

    Blackened
    Signed-off-by: Stephen Suffian <stephen.suffian@pm.me>

--- tests/snapshots/snap_test_database.py
+++ tests/snapshots/snap_test_database.py
@@ -7,29 +7,29 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['test_isd_file_metadata_table_content data'] = {
-    'usaf_id': '690150',
-    'wban_id': '93121',
-    'year': '2006'
+snapshots["test_isd_file_metadata_table_content data"] = {
+    "usaf_id": "690150",
+    "wban_id": "93121",
+    "year": "2006",
 }
 
-snapshots['test_isd_file_metadata_table_count count'] = 44816
+snapshots["test_isd_file_metadata_table_count count"] = 44816
 
-snapshots['test_isd_station_metadata_table_content data'] = {
-    'ba_climate_zone': 'Very Cold',
-    'ca_climate_zone': None,
-    'elevation': '+0032.5',
-    'icao_code': 'PATO',
-    'iecc_climate_zone': '7',
-    'iecc_moisture_regime': None,
-    'latitude': '+60.784',
-    'longitude': '-148.848',
-    'name': 'PORTAGE GLACIER',
-    'quality': 'high',
-    'recent_wban_id': '26492',
-    'state': 'AK',
-    'usaf_id': '700001',
-    'wban_ids': '26492,99999'
+snapshots["test_isd_station_metadata_table_content data"] = {
+    "ba_climate_zone": "Very Cold",
+    "ca_climate_zone": None,
+    "elevation": "+0032.5",
+    "icao_code": "PATO",
+    "iecc_climate_zone": "7",
+    "iecc_moisture_regime": None,
+    "latitude": "+60.784",
+    "longitude": "-148.848",
+    "name": "PORTAGE GLACIER",
+    "quality": "high",
+    "recent_wban_id": "26492",
+    "state": "AK",
+    "usaf_id": "700001",
+    "wban_ids": "26492,99999",
 }
 
-snapshots['test_isd_station_metadata_table_count count'] = 4821
+snapshots["test_isd_station_metadata_table_count count"] = 4821
--- tests/snapshots/snap_test_ranking.py
+++ tests/snapshots/snap_test_ranking.py
@@ -7,146 +7,102 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['test_rank_stations_is_cz2010 is_cz2010=False'] = (
-    4735,
-    15
-)
+snapshots["test_rank_stations_is_cz2010 is_cz2010=False"] = (4735, 15)
 
-snapshots['test_rank_stations_is_cz2010 is_cz2010=True'] = (
-    86,
-    15
-)
+snapshots["test_rank_stations_is_cz2010 is_cz2010=True"] = (86, 15)
 
-snapshots['test_rank_stations_is_tmy3 is_tmy3=False'] = (
-    3802,
-    15
-)
+snapshots["test_rank_stations_is_tmy3 is_tmy3=False"] = (3802, 15)
 
-snapshots['test_rank_stations_is_tmy3 is_tmy3=True'] = (
-    1019,
-    15
-)
+snapshots["test_rank_stations_is_tmy3 is_tmy3=True"] = (1019, 15)
 
-snapshots['test_rank_stations_match_climate_zones_not_null match_ba_climate_zone'] = (
+snapshots["test_rank_stations_match_climate_zones_not_null match_ba_climate_zone"] = (
     282,
-    15
+    15,
 )
 
-snapshots['test_rank_stations_match_climate_zones_not_null match_ca_climate_zone'] = (
+snapshots["test_rank_stations_match_climate_zones_not_null match_ca_climate_zone"] = (
     10,
-    15
+    15,
 )
 
-snapshots['test_rank_stations_match_climate_zones_not_null match_iecc_climate_zone'] = (
+snapshots["test_rank_stations_match_climate_zones_not_null match_iecc_climate_zone"] = (
     742,
-    15
+    15,
 )
 
-snapshots['test_rank_stations_match_climate_zones_not_null match_iecc_moisture_regime'] = (
-    714,
-    15
-)
+snapshots[
+    "test_rank_stations_match_climate_zones_not_null match_iecc_moisture_regime"
+] = (714, 15)
 
-snapshots['test_rank_stations_match_climate_zones_null match_ba_climate_zone'] = (
+snapshots["test_rank_stations_match_climate_zones_null match_ba_climate_zone"] = (
     1478,
-    15
+    15,
 )
 
-snapshots['test_rank_stations_match_climate_zones_null match_ca_climate_zone'] = (
+snapshots["test_rank_stations_match_climate_zones_null match_ca_climate_zone"] = (
     4589,
-    15
+    15,
 )
 
-snapshots['test_rank_stations_match_climate_zones_null match_iecc_climate_zone'] = (
+snapshots["test_rank_stations_match_climate_zones_null match_iecc_climate_zone"] = (
     1475,
-    15
+    15,
 )
 
-snapshots['test_rank_stations_match_climate_zones_null match_iecc_moisture_regime'] = (
+snapshots["test_rank_stations_match_climate_zones_null match_iecc_moisture_regime"] = (
     1906,
-    15
+    15,
 )
 
-snapshots['test_rank_stations_match_state site_state=CA, match_state=False'] = (
+snapshots["test_rank_stations_match_state site_state=CA, match_state=False"] = (
     4821,
-    15
+    15,
 )
 
-snapshots['test_rank_stations_match_state site_state=CA, match_state=True'] = (
-    297,
-    15
-)
+snapshots["test_rank_stations_match_state site_state=CA, match_state=True"] = (297, 15)
 
-snapshots['test_rank_stations_match_state site_state=None, match_state=True'] = (
+snapshots["test_rank_stations_match_state site_state=None, match_state=True"] = (
     1117,
-    15
+    15,
 )
 
-snapshots['test_rank_stations_max_difference_elevation_meters max_difference_elevation_meters=200'] = (
-    4821,
-    15
-)
+snapshots[
+    "test_rank_stations_max_difference_elevation_meters max_difference_elevation_meters=200"
+] = (4821, 15)
 
-snapshots['test_rank_stations_max_difference_elevation_meters site_elevation=0, max_difference_elevation_meters=200'] = (
-    2745,
-    15
-)
+snapshots[
+    "test_rank_stations_max_difference_elevation_meters site_elevation=0, max_difference_elevation_meters=200"
+] = (2745, 15)
 
-snapshots['test_rank_stations_max_difference_elevation_meters site_elevation=0, max_difference_elevation_meters=50'] = (
-    1651,
-    15
-)
+snapshots[
+    "test_rank_stations_max_difference_elevation_meters site_elevation=0, max_difference_elevation_meters=50"
+] = (1651, 15)
 
-snapshots['test_rank_stations_max_difference_elevation_meters site_elevation=1000, max_difference_elevation_meters=50'] = (
-    51,
-    15
-)
+snapshots[
+    "test_rank_stations_max_difference_elevation_meters site_elevation=1000, max_difference_elevation_meters=50"
+] = (51, 15)
 
-snapshots['test_rank_stations_max_distance_meters max_distance_meters=200000'] = (
+snapshots["test_rank_stations_max_distance_meters max_distance_meters=200000"] = (
     49,
-    15
+    15,
 )
 
-snapshots['test_rank_stations_max_distance_meters max_distance_meters=50000'] = (
-    5,
-    15
-)
+snapshots["test_rank_stations_max_distance_meters max_distance_meters=50000"] = (5, 15)
 
-snapshots['test_rank_stations_minimum_quality minimum_quality=high'] = (
-    1627,
-    15
-)
+snapshots["test_rank_stations_minimum_quality minimum_quality=high"] = (1627, 15)
 
-snapshots['test_rank_stations_minimum_quality minimum_quality=low'] = (
-    4821,
-    15
-)
+snapshots["test_rank_stations_minimum_quality minimum_quality=low"] = (4821, 15)
 
-snapshots['test_rank_stations_minimum_quality minimum_quality=medium'] = (
-    1740,
-    15
-)
+snapshots["test_rank_stations_minimum_quality minimum_quality=medium"] = (1740, 15)
 
-snapshots['test_rank_stations_minimum_tmy3_class minimum_tmy3_class=I'] = (
-    222,
-    15
-)
+snapshots["test_rank_stations_minimum_tmy3_class minimum_tmy3_class=I"] = (222, 15)
 
-snapshots['test_rank_stations_minimum_tmy3_class minimum_tmy3_class=II'] = (
-    857,
-    15
-)
+snapshots["test_rank_stations_minimum_tmy3_class minimum_tmy3_class=II"] = (857, 15)
 
-snapshots['test_rank_stations_minimum_tmy3_class minimum_tmy3_class=III'] = (
-    1019,
-    15
-)
+snapshots["test_rank_stations_minimum_tmy3_class minimum_tmy3_class=III"] = (1019, 15)
 
-snapshots['test_rank_stations_no_filter df.shape'] = (
-    4821,
-    15
-)
+snapshots["test_rank_stations_no_filter df.shape"] = (4821, 15)
 
-snapshots['test_select_station_with_empty_tempC station_id'] = '747020'
+snapshots["test_select_station_with_empty_tempC station_id"] = "747020"
 
-snapshots['test_select_station_with_second_level_dates station_id'] = '747020'
+snapshots["test_select_station_with_second_level_dates station_id"] = "747020"
